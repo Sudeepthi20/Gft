0b6a3a68100e7ab27d8abe096efa3966b1360454
Maddipudi <sudeepthi.maddipudi@accenture.com>
2022-02-07 14:05:54 +0530

    first commit

---
+++ src/main/java/com/example/springbootdatamongodb/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.springbootdatamongodb.repository;
+
+import java.util.List;
+
+import com.example.springbootdatamongodb.model.Tutorial;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String>{
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
